f90d29f36ac90bb9949a63d6a168c443bf149e1e
Oscar Arambula <oscar.arambula4388@alumnos.udg.mx>
2022-02-10 21:21:53 -0600

    Eliminandos mensajes de prueba

--- conection.php
+++ conection.php
@@ -4,12 +4,7 @@ $username = "root";
 $password = "";
 $database = "conn-db-php";
 
-try {
-    $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
+$conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully";
-  } catch(PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
-  }
 ?>
